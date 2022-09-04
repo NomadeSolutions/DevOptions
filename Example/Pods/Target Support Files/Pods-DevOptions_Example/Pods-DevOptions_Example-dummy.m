70e74a02582d564054faceff1f692e5ec1d5f42d
Mathieu Juneau <>
2022-09-04 15:50:21 -0400

    Remove Pod Folder in example

--- Example/Pods/Target Support Files/Pods-DevOptions_Example/Pods-DevOptions_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DevOptions_Example : NSObject
-@end
-@implementation PodsDummy_Pods_DevOptions_Example
-@end
--- Example/Pods/Target Support Files/TRZSlideLicenseViewController/TRZSlideLicenseViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TRZSlideLicenseViewController : NSObject
-@end
-@implementation PodsDummy_TRZSlideLicenseViewController
-@end
